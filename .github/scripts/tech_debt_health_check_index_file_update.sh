0a998abff49719f5df98f4c702285c3ce91b717f
Katie Barnett <katie.barnett@bilue.com.au>
2024-11-19 11:19:28 +1100

    Health check report - Index file - append top

--- .github/scripts/tech_debt_health_check_index_file_update.sh
+++ .github/scripts/tech_debt_health_check_index_file_update.sh
@@ -6,21 +6,21 @@ if [[ -z ${FILENAME} ]]; then
     exit 1
 fi
 
-INDEX_FILENAME="Health Checks Releases.md"
+INDEX_FILENAME="Health Checks Reports.md"
 FILENAME_WITHOUT_EXTENSION="${FILENAME//\.md/}"
 
 echo "Filename without extension: $FILENAME_WITHOUT_EXTENSION"
 
 if [ ! -f "$INDEX_FILENAME" ]; then
   echo "Index file not found. Creating."
-  echo "# Health Check Reports" >> "$INDEX_FILENAME"
+  echo "" >> "$INDEX_FILENAME"
 else
   echo "Index file is available. Removing previous checks for this branch."
   sed -i '/$FILENAME_WITHOUT_EXTENSION/d' "$INDEX_FILENAME"
 fi
 
-#Append new file link
-echo -e "- [[$FILENAME_WITHOUT_EXTENSION]] - Updated: $(date +'%Y-%m-%d %H:%M:%S')\n" >> "$INDEX_FILENAME"
+#Append new file link to top of file
+echo -e "- [[$FILENAME_WITHOUT_EXTENSION]] - Updated: $(date +'%Y-%m-%d %H:%M:%S')\n" | cat - "$INDEX_FILENAME"> temp && mv temp "$INDEX_FILENAME"
 
 echo -e "\nDone."
 exit $exit_code
