0bbded1946e2dee99a6d6e60bc50e1c554386a5e
Katie Barnett <katie.barnett@bilue.com.au>
2024-11-19 11:04:50 +1100

    Health check report - Index file script - last part of branch name

--- .github/scripts/tech_debt_health_check_get_report_name.sh
+++ .github/scripts/tech_debt_health_check_get_report_name.sh
@@ -7,15 +7,9 @@ if [[ -z ${GITHUB_REF} ]]; then
 fi
 
 echo "Current branch: $GITHUB_REF"
-if [[ $GITHUB_REF = *"android/release/"* ]]; then
-  echo "In a release branch"
-  IFS='/' read -ra REF_ARRAY <<< "$GITHUB_REF"
-  RELEASE_NUMBER=${REF_ARRAY[-1]}
-  echo "Release number: $RELEASE_NUMBER"
-  echo "REPORT_NAME=Health check report - $RELEASE_NUMBER.md" >> $GITHUB_ENV
-else
-  echo "REPORT_NAME=Health check report - $branch_name.md" >> $GITHUB_OUTPUT
-fi
+IFS='/' read -ra REF_ARRAY <<< "$GITHUB_REF"
+LAST_PART_OF_BRANCH=${REF_ARRAY[-1]}
+echo "REPORT_NAME=Health check report - $LAST_PART_OF_BRANCH.md" >> $GITHUB_OUTPUT
 echo "Report name is: $REPORT_NAME"
 
 export REPORT_NAME
